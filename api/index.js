410160d7ad7570120b3bbead7faa2c9ae3ece357
fiskgrodan <jam@Johans-MacBook-Pro.local>
2022-09-11 23:16:53 +0200

    refactor: use express

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const app = require("express")();
+const { v4 } = require("uuid");
+
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=10, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get("/api/item/:slug", (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+app.get("/api/hello/:name", (req, res) => {
+  const { name = "World" } = req.params;
+  res.end(`Hello ${name}!`);
+});
+
+module.exports = app;
